44ce12dcc9ea4323c76afd560d228a541884f54a
metadoks <52460182+metadoks@users.noreply.github.com>
2026-08-15 19:43:30 +0300

    feat(benchmarks): export mid-ecdf contracts

--- sitescore-benchmarks/src/sitescore_benchmarks/__init__.py
+++ sitescore-benchmarks/src/sitescore_benchmarks/__init__.py
@@ -3,6 +3,7 @@ from .contracts import *
 from .measurement import *
 from .population import *
 from .distribution import *
+from .ecdf import *
 from .evaluation import evaluate_commercial_eligibility, evaluate_boundary_membership
 from .builders import build_frame, derive_lattice_cell
 from .hashing import semantic_hash, GRAMMAR, GRAMMAR_VERSION
